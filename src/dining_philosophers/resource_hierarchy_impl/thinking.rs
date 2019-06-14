bdaba33bcf72c35cb9f9a7b7f54acc982ebc5f1d
Rajiv Kurien <rajiv.kurien@zuhlke.com>
2019-06-14 11:15:57 +0100

    Remove implementation of Thinking

--- src/dining_philosophers/resource_hierarchy_impl/thinking.rs
+++ src/dining_philosophers/resource_hierarchy_impl/thinking.rs
@@ -1,8 +1,8 @@
 use crate::dining_philosophers::fork::Fork;
+use crate::dining_philosophers::philosopher::state_machine::{State, StateMachine};
 use crate::dining_philosophers::resource_hierarchy_impl::left_thinking::LeftThinking;
 use crate::dining_philosophers::resource_hierarchy_impl::right_thinking::RightThinking;
 use crate::dining_philosophers::table::TableInteraction;
-use crate::dining_philosophers::philosopher::state_machine::{StateMachine, State};
 
 #[derive(Debug, PartialEq)]
 pub struct Thinking {
@@ -11,35 +11,13 @@ pub struct Thinking {
 
 impl Thinking {
     pub fn new(table_interaction: TableInteraction) -> Thinking {
-        Thinking {
-            table_interaction: Some(table_interaction),
-        }
-    }
-    fn take_left(&mut self, fork: Fork, seating_position: TableInteraction) -> LeftThinking<> {
-        LeftThinking::new(fork, seating_position)
-    }
-    fn take_right(&mut self, fork: Fork, seating_position: TableInteraction) -> RightThinking<> {
-        RightThinking::new(fork, seating_position)
+        unimplemented!()
     }
 }
 
 impl StateMachine for Thinking {
     fn transition(&mut self) -> Box<StateMachine + Send> {
-        match self.table_interaction.take() {
-            None => { panic!("No longer valid") }
-            Some(t) => {
-                match t.get_left_fork() {
-                    None => {
-                        debug!("{}: Still thinking", t.position);
-                        Box::new(Thinking::new(t))
-                    }
-                    Some(fork) => {
-                        debug!("{}: Got the left one!", t.position);
-                        Box::new(self.take_left(fork, t))
-                    }
-                }
-            }
-        }
+        unimplemented!()
     }
 
     fn state(&self) -> State {
@@ -52,29 +30,11 @@ mod tests {
     use std::sync::{Arc, Mutex};
 
     use crate::dining_philosophers::fork::Fork;
+    use crate::dining_philosophers::philosopher::state_machine::{State, StateMachine};
     use crate::dining_philosophers::resource_hierarchy_impl::left_thinking::LeftThinking;
     use crate::dining_philosophers::resource_hierarchy_impl::right_thinking::RightThinking;
-    use crate::dining_philosophers::table::{Table, TableInteraction};
     use crate::dining_philosophers::resource_hierarchy_impl::thinking::Thinking;
-    use crate::dining_philosophers::philosopher::state_machine::{StateMachine, State};
-
-    #[test]
-    fn take_left_becomes_left_thinking() {
-        let table_interaction = TableInteraction { position: 0, table: Arc::new(Mutex::new(Table::new(1))) };
-        let mut unit = Thinking::new(table_interaction);
-        let table_interaction = unit.table_interaction.take().unwrap();
-
-        assert_eq!(unit.take_left(Fork, table_interaction), LeftThinking::new(Fork, TableInteraction { position: 0, table: Arc::new(Mutex::new(Table::new(1))) }));
-    }
-
-    #[test]
-    fn take_right_becomes_right_thinking() {
-        let table_interaction = TableInteraction { position: 0, table: Arc::new(Mutex::new(Table::new(1))) };
-        let mut unit = Thinking::new(table_interaction);
-        let table_interaction = unit.table_interaction.take().unwrap();
-
-        assert_eq!(unit.take_right(Fork, table_interaction), RightThinking::new(Fork, TableInteraction { position: 0, table: Arc::new(Mutex::new(Table::new(1))) }));
-    }
+    use crate::dining_philosophers::table::{Table, TableInteraction};
 
     #[test]
     fn state_is_thinking() {
@@ -86,25 +46,12 @@ mod tests {
 
     #[test]
     fn changes_to_left_when_left_fork_available() {
-        let table = Table::new(1);
-        let table_interaction = table.get_interactions().pop().unwrap();
-        let mut unit: Box<StateMachine> = Box::new(Thinking::new(table_interaction));
-
-        unit = unit.transition();
-
-        assert_eq!(unit.state(), State::LeftThinking);
+        // Write test here
     }
 
     #[test]
     fn changes_to_thinking_when_left_fork_is_not_available() {
-        let table = Table::new(1);
-        let table_interaction = table.get_interactions().pop().unwrap();
-        table_interaction.get_left_fork();
-        let mut unit: Box<StateMachine> = Box::new(Thinking::new(table_interaction));
-
-        unit = unit.transition();
-
-        assert_eq!(unit.state(), State::Thinking);
+        // Write test here
     }
 
     #[test]
